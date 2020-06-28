1d196eb064f667910790a6f5a078f6ae64d1807d
daipthraomng <52918894+daipthraomng@users.noreply.github.com>
2020-06-28 23:36:09 +0700

    #1 #4 MODIFY bố cục; ADD âm thanh nhạc nền 28/06/2020 23:36

--- AddMake10.js
+++ AddMake10.js
@@ -3,11 +3,29 @@
  */
 var img_cross_line = '<img src="img/obj/apple_cross_line.png">';
 var total_obj = 4; 
+var result_of_question = 2;
+var soundtrack = document.getElementById("soundtrack");
+var true_answer = document.getElementById("true_answer");
+var false_answer = document.getElementById("false_answer");
 
+// xử lý gạch chéo đối tượng đã chọn
 function crossLine(objClick) {
     console.log("click receive " + objClick);
     var obj = document.getElementById(objClick);
     obj.innerHTML = img_cross_line;
     total_obj--;
+}
+
+// kiểm tra kết quả đúng hay sai
+function checkResult() {
+    soundtrack.pause();
+    if (total_obj == result_of_question) {
+        console.log("Congratulation!!!");
+        //window.location="PassGame.html";
+        true_answer.play();
+    }
+    else {
+        console.log("lam lai di");
+    }
 }
 
