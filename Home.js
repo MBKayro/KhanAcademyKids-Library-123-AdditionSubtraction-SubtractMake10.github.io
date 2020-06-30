f511bec5b976cbde6c29c148b7a30554cbdc4065
daipthraomng <52918894+daipthraomng@users.noreply.github.com>
2020-06-30 11:45:15 +0700

    #10 MODIFY chỉnh các src tới img từ "/img..." thành "./img..." tránh việc lỗi code trên gitpage

--- Home.js
+++ Home.js
@@ -3,7 +3,7 @@ var obj = "books"; // đối tượng đã chọn
 // đổi ảnh của tiêu đề được chọn hiện tại
 function choose(objClick) {
     // thay đổi ảnh của tiêu đề được chọn trước đó thành không được chọn
-    document.getElementById(obj).innerHTML = "<img src=\"img/home/library_" + obj + ".png\">";
+    document.getElementById(obj).innerHTML = "<img src=\"./img/home/library_" + obj + ".png\">";
     document.getElementById(obj).setAttribute("onclick", "choose('" + obj + "')");
     if (obj == "123") {
         document.getElementById("select_game").innerHTML= "";
@@ -11,7 +11,7 @@ function choose(objClick) {
     }
     obj = objClick;
     console.log("click receive " + objClick);
-    document.getElementById(objClick).innerHTML = "<img src=\"img/home/library_" + objClick + "_choosed.png\">";
+    document.getElementById(objClick).innerHTML = "<img src=\"./img/home/library_" + objClick + "_choosed.png\">";
     document.getElementById(objClick).setAttribute("onclick", "");
 
     if (obj == "123") {
